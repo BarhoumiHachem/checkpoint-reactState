fdd632ac29e2b5ac0c6e8ddeb9bfa22fc4bdcdc4
BarhoumiHachem <barhoumi.hachem.10@gmail.com>
2022-12-05 12:35:55 +0100

    react state

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import { Button } from 'react-bootstrap';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      fullName: "Hachem Barhoumi",
+      bio: "holder of a degree in computer science and a degree in fullstack js, Passionate about web development. Self-taught and curious.",
+      imgSrc: './photo.jpg',
+      profession: "Web Developer",
+      shows: true,
+      count: 0,
+    }
+  }
+  btnClic = () => {
+    this.setState({ shows: !(this.state.shows),count:0 })
+  }
+
+  componentDidMount() {
+  setInterval (() => {
+      this.setState({count: (this.state.count +1)});
+    }, 1000);}
+
+  render() {
+    return (
+      <div className='App'>
+        {this.state.shows ?
+          (<Button className='btn' variant="success" onClick={this.btnClic}>show profil</Button>)
+          :
+          (
+            <div className='App'>
+            <Button className='btn' variant="danger" onClick={this.btnClic}>hidden profil</Button>
+            <h3>The component has been rendered for {this.state.count} seconds</h3>
+            <h1 className='App-title'>{this.state.fullName}</h1>
+            <p className='App.bio'>{this.state.bio}</p>
+            <img className='App-photo' src={this.state.imgSrc} />
+            <h3 className='App-prof'>{this.state.profession}</h3>
+          </div>)
+        }
+
+      </div>
+    );
+  }
 }
 
 export default App;
